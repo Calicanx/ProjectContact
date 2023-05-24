a07cd3d8727fba0ced47b97a391f57d5e78fb233
Yitzhak Rabin <55562628+Calicanx@users.noreply.github.com>
2023-05-24 09:35:46 +0300

    Update ContactCard.js

--- src/ProjectContact/ContactCard.js
+++ src/ProjectContact/ContactCard.js
@@ -8,70 +8,60 @@ export default function ContactCard({ contact }) {
   const [showDetails, setShowDetails] = useState(false);
   const [showUserDetails, setShowUserDetails] = useState(false);
 
-  const handleCompanyDetails = useCallback(() => {
+  const handleCompanyDetails = () => {
+    setShowDetails(false)
     setShowDetails(true);
     setShowUserDetails(false);
-  }, []);
+  };
 
-  const handleUserDetails = useCallback(() => {
+  const handleUserDetails = () => {
     setShowUserDetails(true);
     setShowDetails(false);
-  }, []);
+  };
+
+  const UserDetails = useCallback(() => {
+    if (showUserDetails) {
+      return (
+          <div className='boxshadow'>
+            <img src={button} className='x' onClick={() => setShowUserDetails(false)}></img>
+            <p><span className='username1'>Name:</span> {name}</p>
+            <p><span className='username1'>Email:</span> {email}</p>
+            <p><span className='username1'>Phone:</span> {phone}</p>
+            <p><span className='username1'>Address:</span> {address.city}</p>
+            <p><span className='username1'>Website:</span> <a href={website}>{website}</a></p>
+          </div>
+      );
+    }
+   return null;
+  }, [showUserDetails, name, email, phone, address, website]);
 
   const CompanyDetails = useCallback(() => {
     if (showDetails) {
       return (
-        <div className='row'>
-          <div className=' col-12 boxshadow'>
-            <img src={button} className='x' onClick={closeCompanydetails}></img>     
+          <div className='boxshadow'>
+            <img src={button} className='x' onClick={() => setShowDetails(false)}></img>     
             <p><span className='companyname'>Company Name:</span> {company.name}</p>
             <p><span className='companyname'>Details:</span> {company.catchPhrase}</p>
             <p><span className='companyname'>Company Industry:</span> {company.bs}</p>
             </div>
-          </div>
       );
     }
     return null;
   }, [showDetails, company]);
 
-  const UserDetails = useCallback(() => {
-    if (showUserDetails) {
-      return (
-        <div className='row'>
-          <div className='col-12 boxshadow'>
-            <img src={button} className='x' onClick={closeUserdetails}></img>
-            <p><span className='username1'>Name:</span> {name}</p>
-            <p><span className='username1'>Email:</span> {email}</p>
-            <p><span className='username1'>Phone:</span> {phone}</p>
-            <p><span className='username1'>Address:</span> {address.city}</p>
-            <p><span className='username1'>Website:</span>{website}</p>
-          </div>
-        </div>
-      );
-    }
-    return null;
-  }, [showUserDetails, name, email, phone, address]);
-
-  const closeCompanydetails = useCallback(() => {
-    setShowDetails(false);
-  })
-
-  const closeUserdetails = useCallback(() => {
-    setShowUserDetails(false);
-  })
-
   return (
     <div className='row'>
       <div className='col-12'>
-        <div className='hq'>
+        <div className='hq col-12'>
           <img src={profilepic} onClick={handleUserDetails} className='profilep'></img>
           <UserDetails />
           <h2 className='username'>{username.toUpperCase()}</h2>
           <h2 className='company'><span className='sp'>Company:</span> {company.name}</h2>
           <button className='contactbtn' onClick={handleCompanyDetails}>Company details</button>
           <CompanyDetails />
-          <hr/>
         </div>
+        <div className='col-11 fs'></div>
+        <div className='hl'><hr/></div>
       </div>
     </div>
 
